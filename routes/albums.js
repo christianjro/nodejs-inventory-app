75ecdd19a83ab5a4dbd45e7c92efe3efdacd444e
Christian Rodriguez <crodriguezochoa@ucdavis.edu>
2022-08-03 20:39:02 -0700

    Album Route - View, Edit, Delete and Bootstrap styling

--- routes/albums.js
+++ routes/albums.js
@@ -1,3 +1,4 @@
+const { render } = require('ejs');
 const express = require('express');
 const router = express.Router();
 const Album = require('../models/album');
@@ -7,23 +8,23 @@ const Genre = require('../models/genre');
 // All Albums Route
 router.get('/', async (req, res) => {
     try {
-        const albums = await Album.find({})
-        res.render('albums/index', {albums: albums})
+        const albums = await Album.find({});
+        res.render('albums/index', {albums: albums});
     } catch {
-        res.redirect('/')
-    }
-})
+        res.redirect('/');
+    };
+});
 
 // New Album Route
 router.get('/new', async (req, res) => {
     try {
-        const genres = await Genre.find({})
-        const artists = await Artist.find({})
-        res.render('albums/new', {artists: artists, genres: genres, album: new Album()})
+        const genres = await Genre.find({});
+        const artists = await Artist.find({});
+        res.render('albums/new', {artists: artists, genres: genres, album: new Album()});
     } catch {
-        res.redirect('/albums')
-    }
-})
+        res.redirect('/albums');
+    };
+});
 
 // Posting New Album to DB
 router.post('/', async (req, res) => {
@@ -34,18 +35,85 @@ router.post('/', async (req, res) => {
         description: req.body.description,
         price: req.body.price,
         numberInStock: req.body.numberInStock,
-    })
+    });
     try {
-        const newAlbum = await album.save()
-        res.redirect('/albums')
-        // res.redirect(`/albums/${newAlbum.id}`)
+        const newAlbum = await album.save();
+        res.redirect(`/albums/${newAlbum.id}`)
     } catch {
         // come back to this...
         res.render('albums/new', {
             album: album, 
             errorMessage: 'Error creating Album.'
-        })
-    }
-})
+        });
+    };
+});
 
-module.exports = router
+// View Album Route
+router.get('/:id', async (req, res) => {
+    try {
+        const album = await Album.findById(req.params.id).populate('genre').populate('artist').exec();
+        res.render('albums/show', { album: album });
+    } catch {
+        res.redirect('/');
+    };
+});
+
+// Edit Album Route
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const album = await Album.findById(req.params.id);
+        const artists = await Artist.find({});
+        const genres = await Genre.find({});
+        res.render('albums/edit', { album: album, artists: artists, genres: genres });
+    } catch {
+        res.redirect('/albums');
+    };
+});
+
+// Edit Album on MongoDB
+router.put('/:id', async (req, res) => {
+    let album;
+    try {
+        album = await Album.findById(req.params.id);
+
+        album.title = req.body.title;
+        album.artist = req.body.artist;
+        album.genre = req.body.genre;
+        album.description = req.body.description;
+        album.price = req.body.price;
+        album.numberInStock = req.body.numberInStock;
+
+        await album.save();
+        res.redirect(`/albums/${album.id}`);
+    } catch {
+        if (album != null) {
+            res.redirect('/albums/edit', {
+                album: album, 
+                errorMessage: "Error updating Album."
+            });
+        } else {
+            res.redirect('/');
+        };
+    };
+});
+
+// Delete Album
+router.delete('/:id', async (req, res) => {
+    let album
+    try {
+        album = await Album.findById(req.params.id);
+        await album.remove()
+        res.redirect('/albums')
+    } catch {
+        if (album != null) {
+            res.render('albums/show', {
+                album: album,
+                errorMessage: 'Could not delete album.'
+            });
+        } else {
+            res.redirect('/');
+        };
+    };
+});
+
+module.exports = router;
